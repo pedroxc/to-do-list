5dd50b82632b7ec3ca5fe8ec57a4cc63e14c90ee
Pedro Coelho <pedroxaviercoelho@hmail.com>
2022-11-08 17:52:39 -0300

    building

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Form from "./components/form";
+import TodoItem from "./components/todoItem";
+import List from "@mui/material/List";
+import { Container } from "@mui/material";
+import "./App.css";
+
+// import { Container } from './styles';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="xs" style={{ marginTop: "1em" }}>
+      <Form />
+      <List
+        sx={{ width: "100%", bgcolor: "background.paper", marginTop: "1em" }}
+      >
+        <TodoItem />
+      </List>
+    </Container>
   );
 }
 
